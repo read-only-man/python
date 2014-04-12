697415d9bb8f749fc0ba2967ae2a380a5095a9c0
read-only-man <maeda.ru@gmail.com>
2014-04-12 15:10:30 +0900

    first commitgit add 20140412/list_1.py

---
+++ 20140412/list_1.py
@@ -0,0 +1,11 @@
+'''first list program'''
+ls = ["first","second","third","fourth"]
+print(ls[0])
+print(ls[2])
+print(ls[-1])
+print(ls[-2])
+print(ls[1:-1])
+print(ls[0:3])
+print(ls[-2:-1])
+print(ls[:3])
+print(ls[-2:])
