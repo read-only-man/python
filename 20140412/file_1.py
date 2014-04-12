5642a1dd519008380e672fde45ef11c40028c025
read-only-man <maeda.ru@gmail.com>
2014-04-12 15:42:23 +0900

    simple file test

---
+++ 20140412/file_1.py
@@ -0,0 +1,14 @@
+# coding:utf-8
+'''simple file test'''
+file = open("myfile","w")
+file.write("First line with necessary newline charactor¥n")
+file.write("Second line to write to the file¥n")
+file.close()
+file = open("myfile","r")
+line1 = file.readline()
+line2 = file.readline()
+print(line1,line2)
+file.close()
+
+import os
+os.remove("myfile")
